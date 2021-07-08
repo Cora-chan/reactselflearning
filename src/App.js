fd6b8fc4f54c2ccfb7692509bc10a4a0b8f823e2
Yue <kangyue9323@gmail.com>
2021-07-08 23:06:46 +1000

    add get input value

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+class App extends Component {
+  constructor()
+  {
+    super();
+    this.state={
+      data:'',
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+handle(event)
+{
+  this.setState({
+    data:event.target.value
+  }) 
 }
-
+render() {
+  return (
+    <div>
+    <input type="text" onChange={this.handle.bind(this)} />
+    <h1>{this.state.data}</h1>
+    </div>
+  )
+}
+}
 export default App;
